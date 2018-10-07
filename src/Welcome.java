153812acc287cf7500b86238b964cd354174a8d0
Chizhov Vladislav <v1ad54@yandex.ru>
2018-10-07 13:07:48 +0700

    Коментарии

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,8 +2,11 @@
 public class Welcome {
 
 	public static void main(String[] args) {
+		// Строка для вывода на экран
 		String greeting = "Welcome Core Java!";
+		// Вывод строки на экран
 		System.out.println(greeting);
+		// Вывод символов = под ранее выведенной строкой
 		for (int i = 0; i < greeting.length(); i++)
 			System.out.print("=");
 		System.out.println();
